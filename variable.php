3504c03e91b45158aa22a28fec1a7d7a7ef7bc41
theanik <anwar.anik33@gmail.com>
2020-02-14 00:23:20 +0600

    variable done

--- variable.php
+++ variable.php
@@ -10,6 +10,39 @@ $foo = "bar";
 $coo = "foo";
 $doo = "coo";
 
-echo $$$coo;
+// echo $$$coo > PHP_EOL;
 
+$x = 5;
 
+function myFunc(){
+    global $x;
+    return  $x;
+}
+// echo myFunc();
+
+function inc(){
+    static $z = 0;
+    echo $z . PHP_EOL;
+    $z++; //in normal case after complite this function $z will 0. but in this case it store last value of z
+
+}
+
+inc();
+inc();
+inc();
+inc();
+
+// recursive function
+
+function reCursiv(){
+    static $x = 0;
+    $x++;
+    echo $x;
+
+    if($x<10){
+        reCursiv();
+    }
+    $x--;
+}
+
+reCursiv();
